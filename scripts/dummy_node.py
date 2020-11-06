ae106afc4fbfe66b32e717c3c66620430c103b6b
Miroslav Radojevic <miroslav.radojevic@gmail.com>
2020-11-06 10:56:48 +0100

    Removed copyright header

--- scripts/dummy_node.py
+++ scripts/dummy_node.py
@@ -1,20 +1,4 @@
 #!/usr/bin/env python
-
-# *****************************************************************************
-#                                                                             *
-#       Copyright (c) 2020  Nuctech Netherlands BV                            *
-#                                                                             *
-#   All rights reserved. Nuctech Netherlands source code is an unpublished    *
-#   work and the use of a copyright notice does not imply otherwise.          *
-#   This source code contains confidential, trade secret material             *
-#   of Nuctech Netherlands. Any attempt or participation in deciphering,      *
-#   decoding, reverse engineering or in any way altering the source           *
-#   code is strictly prohibited, unless the prior written consent of          *
-#   Nuctech Netherlands is obtained. This is proprietary and confidential     *
-#   to Nuctech Netherlands.                                                   *
-#                                                                             *
-# *****************************************************************************
-
 import rospy
 
 if __name__ == '__main__':
