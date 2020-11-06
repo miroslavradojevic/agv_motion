d3bbfef90387ea08f0b3f24d12bae1da2d942d99
Miroslav Radojevic <miroslav.radojevic@gmail.com>
2020-11-06 10:58:47 +0100

    Removed copyright header

--- src/dummy_node.cpp
+++ src/dummy_node.cpp
@@ -1,22 +1,6 @@
-//*****************************************************************************
-//                                                                            *
-//      Copyright (c) 2020  Nuctech Netherlands BV                            *
-//                                                                            *
-//  All rights reserved. Nuctech Netherlands source code is an unpublished    *
-//  work and the use of a copyright notice does not imply otherwise.          *
-//  This source code contains confidential, trade secret material             *
-//  of Nuctech Netherlands. Any attempt or participation in deciphering,      *
-//  decoding, reverse engineering or in any way altering the source           *
-//  code is strictly prohibited, unless the prior written consent of          *
-//  Nuctech Netherlands is obtained. This is proprietary and confidential     *
-//  to Nuctech Netherlands.                                                   *
-//                                                                            *
-//*****************************************************************************
-
 #include <ros/ros.h>
 
-int main (int argc, char** argv)
-{
+int main (int argc, char** argv) {
     ros::init(argc, argv, "dummy_cpp");
 
     ros::NodeHandle nh;
